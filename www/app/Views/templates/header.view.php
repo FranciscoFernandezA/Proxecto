b77114ff9b55d7135d85719687214d5744f47b76
FranciscoFernandezA <franfernandez77777@gmail.com>
2024-11-23 16:18:12 +0100

    borre el boton de fullscreen
    borre el boton de fullscreen

--- www/app/Views/templates/header.view.php
+++ www/app/Views/templates/header.view.php
@@ -54,11 +54,6 @@
           <i class="text-danger fas fa-sign-out-alt"></i>
         </a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
